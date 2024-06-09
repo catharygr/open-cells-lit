1ef67a383a9210156e2f3aede3ce6e9aa7e9dd1d
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-09 14:25:53 +0100

    Configurando la app

--- src/pages/productos/productos-page.ts
+++ src/pages/productos/productos-page.ts
@@ -1,6 +1,6 @@
 import { html, LitElement } from 'lit';
 import { PageController } from '@open-cells/page-controller';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
 // @ts-ignore
 @customElement('productos-page')
@@ -9,6 +9,15 @@ export class ProductosPage extends LitElement {
 
   protected createRenderRoot(): HTMLElement | DocumentFragment {
     return this;
+  }
+
+  @state() private _productos: any[] = [];
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.pageController.subscribe('ch_products', (data: any[]) => {
+      this._productos = data;
+    });
   }
 
   render() {
