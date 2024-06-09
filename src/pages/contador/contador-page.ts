e4799890eb59f44432c5683abb6ceca7a9f9618f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-09 17:33:08 +0100

    Trabajando en contador.page.ts

--- src/pages/contador/contador-page.ts
+++ src/pages/contador/contador-page.ts
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state, query } from 'lit/decorators.js';
 import { PageController } from '@open-cells/page-controller';
 
 @customElement('contador-page')
@@ -21,6 +21,30 @@ export class ContadorPage extends LitElement {
       }
 }
   `;
+
+  @state() private _seconds = 300;
+
+  @state() private _start = false;
+
+  @query('#seconds') secondsInput!: HTMLInputElement;
+
+  startCounter() {
+    this._seconds = this.secondsInput.value
+      ? parseInt(this.secondsInput.value)
+      : 300;
+    this._start = true;
+  }
+
+  stopCounter() {
+    this._start = false;
+  }
+
+  resetCounter() {
+    this._start = false;
+    this._seconds = 300;
+    this.secondsInput.value = '300';
+  }
+
   render() {
     return html`
       <div class="container-contador">
@@ -30,11 +54,17 @@ export class ContadorPage extends LitElement {
         <div class="contador">
           <h1>Contador</h1>
           <div>
-            <label>Segundos:</label>
-            <input type="text" id="seconds" />
+            <label for="input-seconds">Segundos:</label>
+            <input type="text" id="input-seconds" value="300" />
           </div>
-          <contador-display segundos="10" play>Display</contador-display>
-          <div class="botones"></div>
+          <contador-display segundos=${this._seconds} ?start=${this._start}
+            >Display</contador-display
+          >
+          <div class="botones">
+            <button @click=${this.startCounter}>Start</button>
+            <button @click=${this.stopCounter}>Stop</button>
+            <button @click=${this.resetCounter}>Reset</button>
+          </div>
         </div>
       </div>
     `;
