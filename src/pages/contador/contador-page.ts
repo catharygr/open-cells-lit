ee21a4c1a88e3c3a775f52c38b3e97a1fb4625da
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-09 16:53:00 +0100

    Trabajando en css del contador.page.ts

--- src/pages/contador/contador-page.ts
+++ src/pages/contador/contador-page.ts
@@ -1,9 +1,42 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { PageController } from '@open-cells/page-controller';
 
 @customElement('contador-page')
 export class ContadorPage extends LitElement {
+  pageController = new PageController(this);
+
+  static styles = css`
+    .container-contador {
+      background-color: pink;
+      max-width: 600px;
+      padding: 2rem;
+      margin: 5rem auto;
+    }
+      .contador {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+      }
+}
+  `;
   render() {
-    return html` <h1>Contador</h1> `;
+    return html`
+      <div class="container-contador">
+        <button @click="${() => this.pageController.navigate('home')}">
+          Go to home page
+        </button>
+        <div class="contador">
+          <h1>Contador</h1>
+          <div>
+            <label>Segundos:</label>
+            <input type="text" id="seconds" />
+          </div>
+          <contador-display segundos="10" play>Display</contador-display>
+          <div class="botones"></div>
+        </div>
+      </div>
+    `;
   }
 }
