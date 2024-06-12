c6e6f92241ae7d8882905389e3715b52ada52271
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-12 11:38:45 +0100

    Trabajando en componente not-found.ts

--- src/pages/not-found/not-found.ts
+++ src/pages/not-found/not-found.ts
@@ -1,12 +1,24 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { PageController } from '@open-cells/page-controller';
 
 @customElement('not-found-page')
 export class NotFoundPage extends LitElement {
+  controller = new PageController(this);
+
+  config = {
+    category: 'frutas',
+    disponibilidad: true,
+    productoId: '1',
+  };
+
   render() {
     return html`
       <h1>404</h1>
       <p>Page not found</p>
+      <button @click=${() => this.controller.navigate('producto', this.config)}>
+        Home
+      </button>
     `;
   }
 }
