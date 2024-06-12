9a3edd6658e1f2fdf17a709d8e516dbc8fcb6d26
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-12 17:14:41 +0100

    Trabajando en el css del  componente header.ts

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,22 +1,22 @@
 import { html, LitElement } from 'lit';
 import { PageController } from '@open-cells/page-controller';
 import { customElement } from 'lit/decorators.js';
-import '../../components/Header.js';
+// import '../../components/header.js';
 
 // @ts-ignore
 @customElement('home-page')
 export class HomePage extends LitElement {
   pageController = new PageController(this);
 
-  // protected createRenderRoot(): HTMLElement | DocumentFragment {
-  //   // @ts-ignore
-  //   return this;
-  // }
+  protected createRenderRoot(): HTMLElement | DocumentFragment {
+    // @ts-ignore
+    return this;
+  }
 
   render() {
     return html`
-      <header-component .pageController="${this.pageController}">
-      </header-component>
+      <!-- <header-component .pageController="${this.pageController}">
+      </header-component> -->
     `;
   }
 }
