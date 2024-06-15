400343890ed52154d27a5f0433338553c6bb3b13
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-15 18:31:16 +0100

    Trabajando en el css de todo-list-pagets

--- src/pages/tareas/todo-item.ts
+++ src/pages/tareas/todo-item.ts
@@ -1,17 +1,36 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
+interface Task {
+  title: string;
+}
+
 @customElement('todo-item')
 export class TodoItem extends LitElement {
   static styles = css`
     .todo-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
       background-color: #f1f1f1;
       padding: 1rem;
       border-radius: 5px;
     }
+    button {
+      margin: 1rem;
+    }
   `;
-  @property({ type: Object }) task = {};
+  @property({ type: Object }) task: Task = { title: '' };
+  @property({ type: Function }) onDelete: Function = () => {};
+
   render() {
-    return html` <div class="todo-item">${this.task}</div> `;
+    return html`
+      <div class="todo-item">
+        <div>
+          ${this.task.title}
+          <button @click=${() => this.onDelete(this.task)}>Eliminar</button>
+        </div>
+      </div>
+    `;
   }
 }
--- src/pages/tareas/todo-list-page.ts
+++ src/pages/tareas/todo-list-page.ts
@@ -3,21 +3,36 @@ import { customElement, property } from 'lit/decorators.js';
 import { PageController } from '@open-cells/page-controller';
 import './todo-item';
 
-interface Task {
-  name: string;
-  description: string;
-}
-
 @customElement('todo-list-page')
 export class TodoListPage extends LitElement {
   pageController = new PageController(this);
   @property({ type: Array }) tasks: Task[] = [];
 
   static styles = css`
-    .todo-list {
+    .form-todo {
       display: flex;
-      justify-content: center;
+      flex-direction: column;
       gap: 1rem;
+      marrgin: 5rem;
+    }
+    // .todo-list {
+    //   display: flex;
+    //   justify-content: center;
+    //   gap: 1rem;
+    // }
+    form {
+      display: flex;
+      gap: 1rem;
+    }
+    input {
+      flex: 1;
+    }
+    .todo-item {
+      display: flex;
+      flex-direction: column;
+
+      padding: 1rem;
+      border-radius: 5px;
     }
   `;
 
@@ -27,9 +42,36 @@ export class TodoListPage extends LitElement {
       this.tasks = [...this.tasks, task];
     });
   }
+
+  handleDelete(task: Task) {
+    this.tasks = this.tasks.filter((t) => t !== task);
+  }
+
+  handleAddTask(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const task = { title: formData.get('name') as string };
+    this.pageController.publish('add_task', task);
+    form.reset();
+  }
+
   render() {
     return html`
-      ${this.tasks.map((task) => html`<todo-item .task=${task}></todo-item>`)}
+      <div class="todo-list">
+        <form class="form-todo" @submit=${this.handleAddTask}>
+          <input name="name" placeholder="Nombre de la tarea" required />
+          <button type="submit">Agregar tarea</button>
+        </form>
+        ${this.tasks.map(
+          (task) =>
+            html`<todo-item
+              class="todo-item"
+              .task=${task}
+              .onDelete=${() => this.handleDelete(task)}
+            ></todo-item>`
+        )}
+      </div>
     `;
   }
 }
