0e180258ac7e7471cc8000e92c85376f0b5accdd
catharygr <103951447+catharygr@users.noreply.github.com>
2024-06-16 12:07:18 +0100

    Trabajando en el css en  not-found-page.ts

--- src/pages/not-found/not-found.ts
+++ src/pages/not-found/not-found.ts
@@ -1,4 +1,4 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { PageController } from '@open-cells/page-controller';
 
@@ -6,19 +6,29 @@ import { PageController } from '@open-cells/page-controller';
 export class NotFoundPage extends LitElement {
   controller = new PageController(this);
 
-  config = {
-    category: 'frutas',
-    disponibilidad: true,
-    productoId: '1',
-  };
-
+  static styles = css`
+    .container-found {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin: 0;
+    }
+    h1 {
+      font-size: 2rem;
+      color: #f00;
+    }
+    p {
+      margin: 0;
+      font-size: 2rem;
+    }
+  `;
   render() {
     return html`
-      <h1>404</h1>
-      <p>Page not found</p>
-      <button @click=${() => this.controller.navigate('producto', this.config)}>
-        Home
-      </button>
+      <div class="container-found">
+        <h1>404</h1>
+        <p>Page not found</p>
+      </div>
     `;
   }
 }
